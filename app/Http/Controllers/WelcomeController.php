82d0b46ccc85487c44841ebce4c455b005ec9296
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2018-07-18 01:23:11 +0000

    top page

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Item;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
