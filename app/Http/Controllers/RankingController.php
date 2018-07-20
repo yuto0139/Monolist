66bff05798d2f2bff76e9f737809fc9af778d428
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2018-07-20 13:33:03 +0000

    have ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -14,4 +14,13 @@ class RankingController extends Controller
             'items' => $items,
         ]);
     }
+    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+        
+        return view('ranking.have', [
+            'items' => $items, 
+        ]);
+    }
 }
